9dc18821c9e76db0335b16abb1aa15c2544bdf7f
HvSimon <a309011@gmail.com>
2016-10-05 00:24:44 +0800

    finish README & 1335

---
+++ BeijingGuards1335.java
@@ -0,0 +1,92 @@
+package UVaOnlineJudgeSolution;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BeijingGuards1335 {
+
+    static int n;
+    static int[] a;
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int i, minTypeNum, maxNeighborAdd;
+
+        while ((n = input.nextInt()) != 0) {
+
+            a = new int[n + 1];
+
+            for (i = 0; i < n; i++) {
+                a[i] = input.nextInt();
+            }
+
+            a[n] = a[0];
+            maxNeighborAdd = a[0] + a[1];
+
+            for (i = 1; i < n; i++) {
+                maxNeighborAdd = AddMax(maxNeighborAdd, a[i], a[i + 1]);
+            }
+
+            if (n % 2 == 0) {
+
+                minTypeNum = maxNeighborAdd;
+                System.out.println(minTypeNum);
+
+            } else {
+                int L, R, M, maxRequir;
+
+                maxRequir = a[0];
+
+                for (i = 1; i < a.length; i++) {
+                    if (a[i] > maxRequir) {
+                        maxRequir = a[i];
+                    }
+                }
+
+                L = maxNeighborAdd;
+                R = 3 * maxRequir;
+
+                while (L < R) {
+                    M = (L + R) / 2;
+                    if (Test(M)) {
+                        R = M;
+                    } else {
+                        L = M + 1;
+                    }
+                }
+                System.out.println(L);
+            }
+        }
+    }
+
+    public static int AddMax(int max, int a, int b) {
+        
+        if (max < a + b) {
+            max = a + b;
+        }
+        return max;
+    }
+
+    public static boolean Test(int val) {
+        
+        int[] left, right;
+        int rNum, lNum;
+        left = new int[n + 1];
+        right = new int[n + 1];
+
+        lNum = left[1] = a[1];
+        rNum = val - left[1];
+
+        for (int i = 2; i <= n; i++) {
+            if (i % 2 != 0) {
+                right[i] = Math.min(rNum - right[i - 1], a[i]); //右邊剩餘格數 和 需求數比較，格數夠就全放，不夠就放滿
+                left[i] = a[i] - right[i]; //多餘的就放到左邊
+            } else {
+                left[i] = Math.min(lNum - left[i - 1], a[i]); //同理
+                right[i] = a[i] - left[i];
+            }
+        }
+        return left[n] == 0;
+    }
+}
--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 這裡將會陸續存放我自己寫的解題答案，可以搭配我的[部落格](http://hvsimon-lin.logdown.com/)觀看解題想法，歡迎任何想法交流或錯誤指證 :))
 
+## 目前完成(部落格詳細說明連結)
+
++ [1335 - Beijing Guards](http://hvsimon-lin.logdown.com/posts/944628-uva1335-beijing-guards)
+
+## 預計完成
+
++ 686 - Goldbach's Conjecture
+
 ## 使用語言
 
 + Java
